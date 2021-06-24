33d41e762a5bb129dc1a90f71ea02e94ba4efe1f
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-06-24 21:48:18 +0530

    Made progress step component

---
+++ day1/main.js
@@ -0,0 +1,52 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    // prev.removeAttribute("disabled");
+    currentActive = circles.length;
+  } else {
+    // next.setAttribute("disabled", true);
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+  actives.forEach((active, index) => {
+    progress.style.width = `${index * 33.33}%`;
+  });
+
+  if (currentActive === 1) {
+    prev.setAttribute("disabled", true);
+  } else {
+    prev.removeAttribute("disabled");
+  }
+
+  if (currentActive === 4) {
+    next.setAttribute("disabled", true);
+  } else {
+    next.removeAttribute("disabled");
+  }
+}
